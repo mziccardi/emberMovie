cb255aafbf5fde63edc4bff3570bbfebcb85e809
Mike Ziccardi <mikeziccardi@gmail.com>
2017-07-10 14:59:09 -0600

    ability to push new movie review into ember store

---
+++ app/components/add-movie.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  store: Ember.inject.service(),
+
+  actions:{
+    addMovie(){
+      const movie = this.getProperties('title', 'date', 'review');
+      this.get('store').createRecord('movie', movie).save()
+    }
+  }
+});
---
+++ mirage/config.js
@@ -0,0 +1,7 @@
+export default function() {
+    this.get('/movies');
+    this.post('/movies');
+    // this.get('/posts/:id');
+    // this.put('/posts/:id'); // or this.patch
+    // this.del('/posts/:id');
+}
