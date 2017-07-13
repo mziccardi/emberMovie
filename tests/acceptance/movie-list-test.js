38e682be1c04ba984fc02cf170670593f57bd0c9
Mike Ziccardi <mikeziccardi@gmail.com>
2017-07-13 12:34:57 -0600

    test for visiting / and creating a movie are passing

--- tests/acceptance/movie-list-test.js
+++ tests/acceptance/movie-list-test.js
@@ -10,3 +10,24 @@ test('visiting /', function(assert) {
     assert.equal(currentURL(), '/movies');
   });
 });
+
+test('The user should be able to create a new movie review', function(assert){
+  visit('/');
+
+  andThen(function(){
+    assert.equal(currentURL(), '/movies')
+  })
+
+  fillIn('.title-input', 'Pootie Tang');
+  fillIn('.date-input', '2005');
+  fillIn('.review-input', 'Written and directed by Louis CK. HILARIOUS');
+  fillIn('.ratings-input', '5');
+  click('.submit-new-btn')
+
+  andThen(function(){
+    assert.equal(find('.card-movie-title').text().trim(), 'Pootie Tang', 'Should show title')
+    assert.equal(find('.card-movie-date').text().trim(), 'Date Released: 2005', 'Should show date')
+    assert.equal(find('.card-movie-review').text().trim(), 'Review: Written and directed by Louis CK. HILARIOUS', 'Should show review')
+    assert.equal(find('.card-movie-rating').text().trim(), 'Rating: 5 ★', 'Should show rating')
+  })
+})
